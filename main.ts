ea23a7b49424e1c3d10099d9ec78e90faf3e1867
Sqwishino <118532471+Sqwishino@users.noreply.github.com>
2022-11-18 14:06:29 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+input.onSound(DetectedSound.Loud, function () {
+    LightsOn = !(LightsOn)
+    if (LightsOn) {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
+let LightsOn = false
+input.setSoundThreshold(SoundThreshold.Loud, 163)
+basic.forever(function () {
+	
+})
